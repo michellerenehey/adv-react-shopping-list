fb0fd59e8ad52d36e0ae2bfb39d9c2cb6d3bb9ae
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-14 15:48:18 -0800

    started styling homepage

---
+++ src/components/Add/AddItem.jsx
@@ -0,0 +1,5 @@
+import './AddItem.css';
+
+export default function AddItem() {
+  return <div>AddItem</div>;
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,14 @@
 import './Home.css';
+import AddItem from '../../components/Add/AddItem';
+import ItemList from '../../components/ItemList/ItemList';
+import foroe from '../../assets/foroe-islands.jpg';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="Home" style={{ backgroundImage: `url(${foroe})` }}>
+      <h1>Self Care Checklist</h1>
+      <AddItem />
+      <ItemList />
+    </div>
+  );
 }
