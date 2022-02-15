cad233378b2077e582fd5d44b3095bf60a718b48
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-14 16:25:05 -0800

    reducer using with add item

--- src/components/Add/AddItem.jsx
+++ src/components/Add/AddItem.jsx
@@ -1,5 +1,20 @@
 import './AddItem.css';
+import { useState } from 'react';
 
-export default function AddItem() {
-  return <div>AddItem</div>;
+export default function AddItem({ addItem }) {
+  const [newItem, setNewItem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewItem('');
+    addItem(newItem);
+  };
+  return (
+    <div className="AddItem">
+      <form className="add-form" onSubmit={handleSubmit}>
+        <input value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+        <button>Add Item</button>
+      </form>
+    </div>
+  );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,54 @@
+import { useReducer } from 'react';
 import './Home.css';
 import AddItem from '../../components/Add/AddItem';
 import ItemList from '../../components/ItemList/ItemList';
 import foroe from '../../assets/foroe-islands.jpg';
 
+// manipulate id numbers
+let nextId = 3;
+
+//set up initial items
+const initialItems = [
+  { id: 0, text: 'Meditate', done: false },
+  { id: 1, text: 'Go for 20 minute walk', done: false },
+  { id: 2, text: 'Drink a glass of water', done: false },
+];
+
+//create reduce function
+function itemsReducer(state, action) {
+  switch (action.type) {
+    case 'added': {
+      return [...state, { id: action.id, text: action.text, done: false }];
+    }
+    default: {
+      throw Error(`unknown action: ${action.type}`);
+    }
+  }
+}
+
+//
 export default function Home() {
+  // useReducer hook to handle state
+  const [items, dispatch] = useReducer(itemsReducer, initialItems);
+  console.log(items);
+  // dispatch in functions
+  const handleAddItem = (text) => {
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text,
+    });
+  };
+
   return (
     <div className="Home" style={{ backgroundImage: `url(${foroe})` }}>
       <h1>Self Care Checklist</h1>
-      <AddItem />
-      <ItemList />
+      <AddItem addItem={handleAddItem} />
+      {items.map((item) => (
+        <div key={item.id}>
+          <p>{item.text}</p>
+        </div>
+      ))}
     </div>
   );
 }
