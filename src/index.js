22bc9b67e9c029492625c8e2cc05bed5db87afd2
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-16 17:17:24 -0800

    added item number to header

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ListProvider from './context/ListContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ListProvider>
+      <App />
+    </ListProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/Layout/Header/Header.jsx
+++ src/views/Layout/Header/Header.jsx
@@ -1,6 +1,16 @@
 import './Header.css';
 import giphy from '../../../assets/giphy.webp';
+import { useList } from '../../../context/ListContext';
 
 export default function Header() {
-  return <div className="Header" style={{ backgroundImage: `url(${giphy})` }}></div>;
+  const { items } = useList();
+  return (
+    <div className="Header" style={{ backgroundImage: `url(${giphy})` }}>
+      {items.length === 1 ? (
+        <span>You have {items.length} thoughtful thing to do today.</span>
+      ) : (
+        <span>You have {items.length} thoughtful things to do today.</span>
+      )}
+    </div>
+  );
 }
